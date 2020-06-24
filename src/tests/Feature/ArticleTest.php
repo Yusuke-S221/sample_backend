caddb1ae96d22e79633da9c40287acc4d709a0f8
YusukeSato <yskprog022151@gmail.com>
2020-06-24 18:40:04 +0900

    indexテスト作成

--- src/tests/Feature/ArticleTest.php
+++ src/tests/Feature/ArticleTest.php
@@ -5,18 +5,34 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Article;
 
 class ArticleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public $articles;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->articles = factory(Article::class, 10)->create();
+    }
+
+    /** @test */
+    public function indexArticleAll(): void
+    {
+        $response = $this->json('GET', 'api/articles');
+        $response->assertStatus(200)
+                 ->assertJsonFragment([
+                     'id' => $this->articles[0]->id,
+                     'title' => $this->articles[0]->title,
+                     'body' => $this->articles[0]->body,
+                 ],[
+                    'id' => $this->articles[5]->id,
+                    'title' => $this->articles[5]->title,
+                    'body' => $this->articles[5]->body,
+                 ]);
     }
 }
